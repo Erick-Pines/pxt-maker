38a4abb96b2bdcf52985ad02872247ecd008ccd8
Erick-Pines <erickterana@gmail.com>
2020-04-08 12:29:09 -0500

    BrainMX

---
+++ libs/brainmx/config.ts
@@ -0,0 +1,19 @@
+namespace config {
+
+    export const PIN_LED = DAL.PA17;
+
+    export const PIN_D6 = DAL.PA20;
+    export const PIN_D8 = DAL.PA06;
+    export const PIN_D10 = DAL.PA18;
+    export const PIN_D11 = DAL.PA16;
+    export const PIN_D12 = DAL.PA19;
+    export const PIN_D13 = DAL.PA17;
+
+    export const PIN_D17 = DAL.PA21;
+        
+    export const PIN_D0 = DAL.PA11;
+    export const PIN_D1 = DAL.PA10;
+    export const PIN_D9 = DAL.PA15;
+
+    // additional pins to look for: SPI, I2C, Flash
+}
---
+++ libs/brainmx/device.d.ts
@@ -0,0 +1,43 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPin(PIN_LED)
+    const LED: PwmOnlyPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_D6)
+    const MA1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D8)
+    const MA2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D10)
+    const MB1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D11)
+    const MB2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D12)
+    const MC1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D13)
+    const MC2: PwmPin;
+    
+    //% fixedInstance shim=pxt::getPin(PIN_D0)
+    const DIN: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D1)
+    const BCLK: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D9)
+    const LRCLK: PwmPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_D17)
+    const NEO: DigitalInOutPin;
+
+/*    const RX: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_TX)
+    const TX: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MISO)
+    const MISO: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_MOSI)
+    const MOSI: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCK)
+    const SCK: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SCL)
+    const SCL: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SDA)
+    const SDA: DigitalInOutPin;*/
+
+}
+
